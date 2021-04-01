b1b08a68a9864d959ffd4010d1a3e78ea3775031
Goryunova <bilka77@mail.ru>
2021-04-01 17:04:03 +0500

    Third commit: new filed added

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 
-from django.forms import Textarea
-
 from .models import Post, Comment
 
 
--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth import get_user_model
 
-from posts.models import Post
-from django.forms import ModelForm
 
 User = get_user_model()
 
